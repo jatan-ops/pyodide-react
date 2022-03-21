af0c9e90acb6f96f7d8726fffcd95b3f8e4d3626
jatan-ops <jatan.shah90@gmail.com>
2022-03-21 22:02:28 +0530

    added state mgmt for dataframe & pivot table

--- src/App.js
+++ src/App.js
@@ -4,36 +4,79 @@ function App() {
 
   const pyodide = useRef(null)
   
+  const [list, setList] = useState([])
   const [pyodideInitialized, setPyodideInitialized] = useState(false)
+
+  function feedData() {
+      const temp = pyodide.current.runPython(`
+        import pandas as pd
+        import json
+
+        df = pd.DataFrame({
+          'Product' : ['Carrots', 'Broccoli', 'Banana', 'Banana','Beans', 'Orange', 'Broccoli', 'Banana'],
+          'Category' : ['Vegetable', 'Vegetable', 'Fruit', 'Fruit','Vegetable', 'Fruit', 'Vegetable', 'Fruit'],
+          'Quantity' : [8, 5, 3, 4, 5, 9, 11, 8],
+          'Amount' : [270, 239, 617, 384, 626, 610, 62, 90]
+        })
+
+        def returnList():
+          list = df.values.tolist()
+          print(list)
+          jsList = json.dumps(list)
+          return jsList
+        
+        returnList()
+      `)
+      setList(temp)
+      console.log('list: ', temp)
+      console.log('Ready to pivot!')
+  }
+
+  function createPivotTable() {
+    const pivotList = pyodide.current.runPython(`
+
+      import pandas as pd
+
+      df = pd.DataFrame(${list})
+      df.columns = ['Product','Category','Quantity','Amount']
+
+      pivot = df.pivot_table(index =['Product'], values =['Amount'], aggfunc ='sum')
+      
+      newPivot=pivot.reset_index()
+      list = newPivot.values.tolist()
+      jsList = json.dumps(list)
+      
+      def returnPivot():
+        return jsList
+      
+      returnPivot()
+    `)
+    console.log('pivotList: ', pivotList)
+  }
+
+  // def returnPivot():
+        
+  //       list = pivot.values.tolist()
+  //       jsList = json.dumps(list)
+  //       return jsList
+      
+  //     returnPivot()
+  
+  //     print(jsList)
 
   useEffect(async () => {
     if(!pyodideInitialized) {
       pyodide.current = await window.loadPyodide({ indexURL : "https://cdn.jsdelivr.net/pyodide/v0.19.1/full/" });
       await pyodide.current.loadPackage("pandas");
       setPyodideInitialized(true)
-      pyodide.current.runPython(`
-        import pandas as pd
-
-        df = pd.DataFrame({'Product' : ['Carrots', 'Broccoli', 'Banana', 'Banana','Beans', 'Orange', 'Broccoli', 'Banana'],
-          'Category' : ['Vegetable', 'Vegetable', 'Fruit', 'Fruit',
-                        'Vegetable', 'Fruit', 'Vegetable', 'Fruit'],
-          'Quantity' : [8, 5, 3, 4, 5, 9, 11, 8],
-          'Amount' : [270, 239, 617, 384, 626, 610, 62, 90]})
-
-        print(df)
-            
-        pivot = df.pivot_table(index =['Product'],
-                       values =['Amount'],
-                       aggfunc ='sum')
-        
-        print(pivot)
-      `); 
+      feedData()
     }
   },[])
 
   return (
     <div className="App">
       Hello Jatan
+      <button onClick={createPivotTable} >Create Pivot table</button>
     </div>
   );
 }
