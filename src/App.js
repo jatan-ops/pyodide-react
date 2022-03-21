7d335afd585aa64c07a344261651b86ba0a14e26
jatan-ops <jatan.shah90@gmail.com>
2022-03-21 19:25:24 +0530

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useRef, useState } from 'react';
 
 function App() {
+
+  const pyodide = useRef(null)
+  
+  const [pyodideInitialized, setPyodideInitialized] = useState(false)
+
+  useEffect(async () => {
+    if(!pyodideInitialized) {
+      pyodide.current = await window.loadPyodide({ indexURL : "https://cdn.jsdelivr.net/pyodide/v0.19.1/full/" });
+      await pyodide.current.loadPackage("pandas");
+      setPyodideInitialized(true)
+      pyodide.current.runPython(`
+        import pandas as pd
+
+        df = pd.DataFrame({'Product' : ['Carrots', 'Broccoli', 'Banana', 'Banana','Beans', 'Orange', 'Broccoli', 'Banana'],
+          'Category' : ['Vegetable', 'Vegetable', 'Fruit', 'Fruit',
+                        'Vegetable', 'Fruit', 'Vegetable', 'Fruit'],
+          'Quantity' : [8, 5, 3, 4, 5, 9, 11, 8],
+          'Amount' : [270, 239, 617, 384, 626, 610, 62, 90]})
+
+        print(df)
+            
+        pivot = df.pivot_table(index =['Product'],
+                       values =['Amount'],
+                       aggfunc ='sum')
+        
+        print(pivot)
+      `); 
+    }
+  },[])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Hello Jatan
     </div>
   );
 }
 
 export default App;
+// used https://www.geeksforgeeks.org/how-to-create-a-pivot-table-in-python-using-pandas/ to create pivot table
